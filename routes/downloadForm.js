3932a77d7333159d6232378cc9bce4a372fbfec8
sy <greyhairchooselife@gmail.com>
2023-01-03 16:49:32 +0900

    (진위여부)대량 조회 활성화(양식 파일 생성 및 다운로드 만들기, 파일 선택하면 유효성 확인하여 피드백, 조회하면 결과 엑셀파일 만들어서 다운로드하기)
    양식 생성 및 다운로드 response 구현

--- routes/downloadForm.js
+++ routes/downloadForm.js
@@ -1,15 +1,30 @@
 var express = require('express');
-var exceljs = require('exceljs');
-var fs = require('fs');
+const fs = require('fs');
 
 var router = express.Router();
 
-/* GET users listing. */
-router.post('/', function(req, res, next) {
+router.post('/', async function(req, res, next) {
+	const ExcelJS = require('exceljs');
+	const workbook = new ExcelJS.Workbook();
+	const sheet = workbook.addWorksheet('1');
+
 	switch(req.body.case) {
 		case 'valid_multi':
-			console.log(req.body.case)
-			res.send('download form router');
+
+			if(!fs.existsSync('downloadDirectory/사업자_진위_여부_양식.xlsx')) {
+				sheet.columns = [
+					{header: 'No.', key: 'number'},
+					{header: '사업자 번호', key: 'businessNumber', width: 15, style: {alignment: {horizontal: 'center'}}},
+					{header: '대표명', key: 'representative', style: {alignment: {horizontal: 'center'}}},
+					{header: '사업 개시일', key: 'startDate', width: 12, style: {alignment: {horizontal: 'center'}}},
+				]
+
+				sheet.addRow({number: 1, businessNumber: '111223333', representative: '홍길동', startDate: '20230101'})
+
+				await workbook.xlsx.writeFile('downloadDirectory/사업자_진위_여부_양식.xlsx')
+			}
+
+			res.download('downloadDirectory/사업자_진위_여부_양식.xlsx');
 			break;
 		case 'state_multi':
 			break;
