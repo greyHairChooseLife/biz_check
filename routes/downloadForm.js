2b3593d46e9751ad5889a17485ba8dd98c814f3d
sy <greyhairchooselife@gmail.com>
2023-01-13 22:16:26 +0900

    (휴폐업)대량 조회 활성화(양식 파일 생성 및 다운로드 만들기, 파일 선택하면 유효성 확인하여 피드백, 조회하면 결과 엑셀파일 만들어서 다운로드하기)
    양식 파일 만들기
    
    분기 방식 변경 : 기존 hidden input의 값으로 나누던 것에서 router 하나 더 만드는 것으로

--- routes/downloadForm.js
+++ routes/downloadForm.js
@@ -3,39 +3,52 @@ const stream = require('stream');
 
 var router = express.Router();
 
-router.post('/', async function(req, res, next) {
+router.get('/valid', async function(req, res, next) {
 	const ExcelJS = require('exceljs');
 	const workbook = new ExcelJS.Workbook();
 	const sheet = workbook.addWorksheet('1');
 
-	switch(req.body.formCase) {
-		case 'valid_multi':
-			//	make file buffer
-			let fileBuffer;
-			sheet.columns = [
-				{header: 'No.', key: 'number'},
-				{header: '사업자 번호', key: 'businessNumber', width: 15, style: {alignment: {horizontal: 'center'}}},
-				{header: '대표명', key: 'representative', style: {alignment: {horizontal: 'center'}}},
-				{header: '사업 개시일', key: 'startDate', width: 12, style: {alignment: {horizontal: 'center'}}},
-				{},
-				{},
-				{width: 80}
-			]
-			sheet.addRow({number: 1, businessNumber: '111223333', representative: '홍길동', startDate: '20230101'})
-			fileBuffer = await workbook.xlsx.writeBuffer();
+	//	make file buffer
+	let fileBuffer;
+	sheet.columns = [
+		{header: 'No.', key: 'number'},
+		{header: '사업자 번호', key: 'businessNumber', width: 15, style: {alignment: {horizontal: 'center'}}},
+		{header: '대표명', key: 'representative', style: {alignment: {horizontal: 'center'}}},
+		{header: '사업 개시일', key: 'startDate', width: 12, style: {alignment: {horizontal: 'center'}}},
+	]
+	sheet.addRow({number: 1, businessNumber: '111223333', representative: '홍길동', startDate: '20230101'})
+	fileBuffer = await workbook.xlsx.writeBuffer();
 
-			const readStream = new stream.PassThrough();
-			readStream.end(fileBuffer)
+	const readStream = new stream.PassThrough();
+	readStream.end(fileBuffer)
 
-			res.set('Content-disposition', 'attachment; filename=' + encodeURI('사업자_진위_여부_양식.xlsx'));
-			res.set('Content-Type', 'text/plain');
+	res.set('Content-disposition', 'attachment; filename=' + encodeURI('사업자_진위_여부_양식.xlsx'));
+	res.set('Content-Type', 'text/plain');
 
-			readStream.pipe(res);
-			break;
+	readStream.pipe(res);
+});
 
-		case 'state_multi':
-			break;
-	}
+router.get('/state', async function(req, res, next) {
+	const ExcelJS = require('exceljs');
+	const workbook = new ExcelJS.Workbook();
+	const sheet = workbook.addWorksheet('1');
+
+	//	make file buffer
+	let fileBuffer;
+	sheet.columns = [
+		{header: 'No.', key: 'number'},
+		{header: '사업자 번호', key: 'businessNumber', width: 15, style: {alignment: {horizontal: 'center'}}},
+	]
+	sheet.addRow({number: 1, businessNumber: '111223333'})
+	fileBuffer = await workbook.xlsx.writeBuffer();
+
+	const readStream = new stream.PassThrough();
+	readStream.end(fileBuffer)
+
+	res.set('Content-disposition', 'attachment; filename=' + encodeURI('휴폐업_상태_조회_양식.xlsx'));
+	res.set('Content-Type', 'text/plain');
+
+	readStream.pipe(res);
 });
 
 module.exports = router;
